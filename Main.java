9daa4f2de5b6649105e44e6c292b371aaa1cad44
LuizGustavoDCarvalho <99940725+LuizGustavoDCarvalho@users.noreply.github.com>
2022-03-03 23:38:37 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int part1 = 130;
+        int part2 = 240;
+        int part3 = 320;
+        double media;
+
+        media = (part1+part2+part3)/3;
+
+        System.out.println("Número de Zumbis mortod: " + (part1+part2+part3));
+        System.out.println("Média de zumbis mortos: "+ media);
+    }
+}
